348f3a64da675219de2138d79ffb9ee5859046b9
bhavyuh15 <bhavyadatey15@gmail.com>
2025-01-13 10:53:15 +0530

    Delete Programming elements day1Level1 directory

--- Programming elements day1Level1/code16L1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class code16L1{
-	public  static void main(String args[]){
-		Scanner input = new Scanner(System.in);
-		System.out.println("enter the number of students");
-		int numberOfStudents = input.nextInt();
-		int possibleHandshakes= (numberOfStudents*(numberOfStudents - 1))/2; // finding the possible Handshakes
-		System.out.println(" possible number of handshakes is " +possibleHandshakes);
-}}
